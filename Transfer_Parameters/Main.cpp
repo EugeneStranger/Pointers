8820fd8354acd8060924e2b3ac386d3c9a7d6773
admin <admin@H>
2023-05-09 12:36:00 +0300

    TransferParametersByPointers

--- Transfer_Parameters/Main.cpp
+++ Transfer_Parameters/Main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void Exchange(int a, int b);
+void Exchange(int* a, int* b);
 
 void main()
 {
@@ -10,14 +10,16 @@ void main()
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
 	cout << &a << "\t" << &b << endl;
-	Exchange(a , b);
+	Exchange(&a , &b);
 	cout << a << "\t" << b << endl;
 	cout << &a << "\t" << &b << endl;
 
 }
-void Exchange(int a, int b)
+void Exchange(int* a, int* b)
 {
-	int buffer = a;
-	a = b;
-	b = buffer;
+	int buffer = *a;
+	*a = *b;
+	*b = buffer;
+	cout << &a << "\t" << &b << endl;
+
 }
